3b3946b8e4dcbd265aa92c8b849f2f962e19e584
Brian Yu <brian.yu@czbiohub.org>
2018-12-09 17:57:45 -0800

    add bedtools and used 1000 window size

--- misc_scripts/Tabulate_BasePair_Coverage.py
+++ misc_scripts/Tabulate_BasePair_Coverage.py
@@ -254,7 +254,10 @@ def analyze_one_sample(reference_fasta, ref_list, bamfile_name, window_size, sam
     alignment_class = Tabulate_BasePair_Coverage(reference_fasta, ref_list)
     if regionsBedFile:
         sampleName = bamfile_name.split('.')[0]
-        call(["bedtools", "intersect", "-abam", bamfile_name, "-v", "-b", regionsBedFile, ">", sampleName+'.regionRemoved.bam'])
+        bedtool_string = 'bedtools intersect -abam '+bamfile_name+' -v -b '+regionsBedFile+' > '+sampleName+'.regionRemoved.bam'
+        print(bedtool_string)
+        call(bedtool_string)
+        # call(["bedtools", "intersect", "-abam", bamfile_name, "-v", "-b", regionsBedFile, ">", sampleName+'.regionRemoved.bam'])
         alignment_class.extract_bam_entries(sampleName+'.regionRemoved.bam', 100) # output 3 bam files in the same folder
     else:
         alignment_class.extract_bam_entries(bam_file, 100)
