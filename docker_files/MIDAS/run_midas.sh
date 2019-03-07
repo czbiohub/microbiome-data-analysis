96e28f7fd77ec9ba42a5c826b52fc44ed97d75ba
Sunit Jain <mail4sunit@gmail.com>
2019-03-06 18:34:53 -0800

    #18 really close now

--- docker_files/MIDAS/run_midas.sh
+++ docker_files/MIDAS/run_midas.sh
@@ -21,15 +21,29 @@
 #       hardTrim=80 OR ""
 #       subsetReads=4000000 OR ""
 #       s3path2db="s3://..." OR empty -> DEFAULT: "s3://czbiohub-brianyu/Synthetic_Community/Genome_References/MIDAS/1.2/midas_db_v1.2.tar.gz"
+#
+#   Sample Batch Submission:
+#   aegea batch submit --queue aegea_batch --image sunitjain/midas:latest \
+#     --storage /mnt=500 --memory 64000 --vcpus 16 \
+#     --command="export coreNum=16; \
+#     export fastq1=s3://czbiohub-brianyu/Original_Sequencing_Data/180727_A00111_0179_BH72VVDSXX/Alice_Cheng/Strain_Verification/Dorea-longicatena-DSM-13814_S275_R1_001.fastq.gz; \
+#     export fastq2=s3://czbiohub-brianyu/Original_Sequencing_Data/180727_A00111_0179_BH72VVDSXX/Alice_Cheng/Strain_Verification/Dorea-longicatena-DSM-13814_S275_R2_001.fastq.gz; \
+#     export s3OutputPath=s3://czbiohub-brianyu/Sunit_Jain/20190304_Midas_Test/Dorea-longicatena-DSM-13814/; \
+#     export subsetReads=''; \
+#     export hardTrim=''; \
+#     ./run_midas.sh"
 ####################################################################
 
 set -e
 set -u
 
 ############################# SETUP ################################
-
+START_TIME=$SECONDS
 # export $@
+export PATH="/opt/conda/bin:${PATH}"
+export PYTHONPATH="/opt/conda/lib/python2.7/site-packages/midas"
 
+echo $PATH
 LOCAL=$(pwd)
 
 OUTPUTDIR=${LOCAL}/tmp_$( date +"%Y%m%d_%H%M%S" )
@@ -38,19 +52,15 @@ QC_FASTQ="${OUTPUTDIR}/trimmed_fastq"
 SPECIES_OUT="${OUTPUTDIR}/midas_output"
 GENES_OUT="${OUTPUTDIR}/midas_output"
 REF_DB="${OUTPUTDIR}/reference"
-# adapterFile="${LOCAL}/data/illumina_adapters.fa"
-adapterFile="illumina_adapters.fa"
 defaultDB="s3://czbiohub-brianyu/Synthetic_Community/Genome_References/MIDAS/1.2/midas_db_v1.2.tar.gz"
+
 # remove trailing '/'
-
-head ${adapterFile}
-
 s3OutputPath=${s3OutputPath%/}
-mkdir -p ${RAW_FASTQ} ${QC_FASTQ} ${SPECIES_OUT} ${GENES_OUT}
+mkdir -p ${OUTPUTDIR} ${RAW_FASTQ} ${QC_FASTQ} ${SPECIES_OUT} ${GENES_OUT}
 
-trap '{ rm -rf ${OUTPUTDIR} ; exit 255; }' EXIT 
+trap '{ rm -rf ${OUTPUTDIR} ; exit 255; }' 1 
 
-######################### Downloads ################################
+############################ DATABASE ##############################
 
 ## Database
 if [[ -z ${s3path2db+defaultDB} ]]; then
@@ -60,27 +70,13 @@ fi
 
 localPath2db_tgz=${REF_DB}/$(basename ${s3path2db})
 localPath2db=${REF_DB}/$(basename ${s3path2db} .tar.gz)
-
-# Download
 aws s3 cp --quiet ${s3path2db} ${localPath2db_tgz}
 
 # Decompress
 tar -xzf ${localPath2db_tgz} --directory ${REF_DB}/
 rm -rf ${localPath2db_tgz}
 
-# Download Fastq
-FASTQ1_FILENAME=$(basename ${fastq1} .gz)
-FASTQ2_FILENAME=$(basename ${fastq2} .gz)
-
-aws s3 cp ${fastq1} ${RAW_FASTQ}/
-gunzip ${RAW_FASTQ}/${FASTQ1_FILENAME}.gz
-
-aws s3 cp ${fastq2} ${RAW_FASTQ}/
-gunzip ${RAW_FASTQ}/${FASTQ2_FILENAME}.gz
-
 ########################## PARAMETERS ##############################
-
-############################## QC ##################################
 
 # Constant definitions for bbduk
 trimQuality=25
@@ -88,38 +84,56 @@ minLength=50
 kmer_value=23
 min_kmer_value=11
 
-####################### MIDAS - Species ############################
-
-# Run MIDAS 
 ## Species
-MIDAS_PARAMS="-t ${coreNum} -d ${localPath2db} --remove_temp"
+MIDAS_SPECIES_PARAMS="-t ${coreNum} -d ${localPath2db} --remove_temp"
+MIDAS_GENES_PARAMS=${MIDAS_SPECIES_PARAMS}
 
 # Use bbduk to trim reads, -eoom exits when out of memory
-BBDUK_PARAMS="in1=${RAW_FASTQ}/${FASTQ1_FILENAME} out1=${QC_FASTQ}/${FASTQ1_FILENAME} ref=adapters ktrim=r k=${kmer_value} mink=${min_kmer_value} hdist=1 tbo qtrim=rl trimq=${trimQuality} minlen=${minLength}"
+BBDUK_PARAMS="ref=adapters ktrim=r k=${kmer_value} mink=${min_kmer_value} hdist=1 tbo qtrim=rl trimq=${trimQuality} minlen=${minLength}"
 
 ### Single end
-if [[ -z ${fastq2} ]]; then
-    MIDAS_PARAMS="${MIDAS_PARAMS} -1 ${QC_FASTQ}/${FASTQ1_FILENAME}"
+if [[ -n ${fastq1} ]]; then
+    FASTQ1_FILENAME=$(basename ${fastq1} .gz)
+    
+    BBDUK_PARAMS="${BBDUK_PARAMS} in1=${RAW_FASTQ}/${FASTQ1_FILENAME} out1=${QC_FASTQ}/${FASTQ1_FILENAME}"
+    MIDAS_SPECIES_PARAMS="${MIDAS_SPECIES_PARAMS} -1 ${QC_FASTQ}/${FASTQ1_FILENAME}"
+    MIDAS_GENES_PARAMS=${MIDAS_SPECIES_PARAMS}
+    # Download
+    aws s3 cp ${fastq1} ${RAW_FASTQ}/
+    gunzip ${RAW_FASTQ}/${FASTQ1_FILENAME}.gz
 else
+    echo "[FATAL] Missing fastq file."
+fi
+
 ### Paired
-    MIDAS_PARAMS="${MIDAS_PARAMS} -1 ${QC_FASTQ}/${FASTQ1_FILENAME} -2 ${QC_FASTQ}/${FASTQ2_FILENAME}"
+if [[ -n ${fastq2:-} ]]; then
+    FASTQ2_FILENAME=$(basename ${fastq2} .gz)
+
     BBDUK_PARAMS="${BBDUK_PARAMS} in2=${RAW_FASTQ}/${FASTQ2_FILENAME} out2=${QC_FASTQ}/${FASTQ2_FILENAME}"
+    MIDAS_SPECIES_PARAMS="${MIDAS_SPECIES_PARAMS} -2 ${QC_FASTQ}/${FASTQ2_FILENAME}"
+    MIDAS_GENES_PARAMS=${MIDAS_SPECIES_PARAMS}
+    # Download
+    aws s3 cp ${fastq2} ${RAW_FASTQ}/
+    gunzip ${RAW_FASTQ}/${FASTQ2_FILENAME}.gz
 fi
 
 ### Hard Trimming
 if [[ -n ${hardTrim:-} ]]; then
-    MIDAS_PARAMS="${MIDAS_PARAMS} --read_length ${hardTrim}"
+    MIDAS_SPECIES_PARAMS="${MIDAS_SPECIES_PARAMS} --read_length ${hardTrim}"
 fi
 
 ### Read subsets
 if [[ -n ${subsetReads:-} ]]; then
-    MIDAS_PARAMS="${MIDAS_PARAMS} -n ${subsetReads}"
+    MIDAS_SPECIES_PARAMS="${MIDAS_SPECIES_PARAMS} -n ${subsetReads}"
 fi
 
-MIDAS="run_midas.py species ${SPECIES_OUT} ${MIDAS_PARAMS}"
-BBDUK="bbduk.sh -Xmx16g -eoom ${BBDUK_PARAMS}"
+########################### EXECUTION ##############################
 
-########################### Execute ################################
+BBDUK="bbduk.sh -Xmx16g -eoom ${BBDUK_PARAMS}"
+MIDAS_SPECIES="run_midas.py species ${SPECIES_OUT} ${MIDAS_SPECIES_PARAMS}"
+MIDAS_GENES="run_midas.py genes ${GENES_OUT} ${MIDAS_GENES_PARAMS}"
+
+############################# BBDUK ################################
 
 if eval "${BBDUK}"; then
     echo "[$(date)] BBDUK complete."
@@ -128,12 +142,31 @@ else
     exit 1;
 fi
 
-if eval "${MIDAS}"; then
-    echo "[$(date)] MIDAS Species complete. Syncing output to ${s3OutputPath}/Species"
-    aws s3 sync --quiet ${SPECIES_OUT}/species/ ${s3OutputPath}/Species/
+####################### MIDAS - Species ############################
+
+if eval "${MIDAS_SPECIES}"; then
+    echo "[$(date)] MIDAS Species complete. Syncing output to ${s3OutputPath}"
+    aws s3 sync ${SPECIES_OUT}/ ${s3OutputPath}/
 else
     echo "[$(date)] MIDAS Species failed."
     exit 1;
 fi
 rm -rf ${SPECIES_OUT}
-# TODO: ####################### MIDAS - Genes ##############################
+
+######################## MIDAS - Genes #############################
+
+if eval "${MIDAS_GENES}"; then
+    echo "[$(date)] MIDAS Genes complete. Syncing output to ${s3OutputPath}"
+    aws s3 sync ${GENES_OUT}/ ${s3OutputPath}/
+else
+    echo "[$(date)] MIDAS Genes failed."
+    exit 1;
+fi
+rm -rf ${GENES_OUT}
+
+######################### HOUSEKEEPING #############################
+DURATION=$((SECONDS - START_TIME))
+hrs=$(( DURATION/3600 )); mins=$(( (DURATION-hrs*3600)/60)); secs=$(( DURATION-hrs*3600-mins*60 ))
+printf 'This AWSome pipeline took: %02d:%02d:%02d\n' $hrs $mins $secs
+echo "Live long and prosper"
+############################ PEACE! ################################
