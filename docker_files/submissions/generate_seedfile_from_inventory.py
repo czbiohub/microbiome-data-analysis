2e10786bf88b77167f6ac5603a346e07f0e281c8
Sunit Jain <mail4sunit@gmail.com>
2021-01-29 13:18:02 -0800

    use S3 inventory to get seedfiles

---
+++ docker_files/submissions/generate_seedfile_from_inventory.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+# Search through czb-seqbot fastq inventory for specific sample names and
+# generate a seedfile for further analysis
+## Also requires pyarrow, fastparquet
+## conda install -y pyarrow fastparquet
+
+# USAGE: python generate_seedfile_from_inventory.py <sample_names.list> <s3://inventory_s3path> <source_bucket_name> <output_file.csv>
+
+
+import logging
+import pandas as pd
+import boto3
+import s3fs
+import io
+import sys
+
+
+def generate_seedfile(inventory_df, samples_list, source_bucket):
+    seedfile_list = list()
+    for sample in samples_list:
+        fwd, rev = sorted(
+            inventory_df[inventory_df["key"].str.contains(sample, na=False)]["key"]
+        )
+        seedfile_list.append(
+            {
+                "sampleName": sample,
+                "R1": f"s3://{source_bucket}/{fwd}",
+                "R2": f"s3://{source_bucket}/{rev}",
+            }
+        )
+
+    return pd.DataFrame(seedfile_list)
+
+
+def read_list_file(list_file):
+    with open(list_file) as items_file:
+        items_list = [i.rstrip("\n") for i in items_file]
+
+    return items_list
+
+
+def split_s3uri(s3uri):
+    s3path_as_list = s3uri.replace("s3://", "").rstrip("/").split("/")
+    bucket = s3path_as_list.pop(0)
+    prefix = "/".join(s3path_as_list)
+
+    return bucket, prefix
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s\t[%(levelname)s]:\t%(message)s",
+    )
+
+    # list of sample names as in the sequencing summary sheet
+    # no headers; one sample name per line
+    samples_list_file = sys.argv[1]
+    # s3 location of the inventory file
+    s3_inventory_path = sys.argv[2]
+    # bucket for which the inventory is being taken
+    seq_bucket = sys.argv[3]
+    # Output file name
+    output_file = sys.argv[4]
+
+    samples_list = read_list_file(samples_list_file)
+    bucket_name, file_name = split_s3uri(s3_inventory_path)
+
+    client = boto3.resource("s3")
+    object = client.Object(bucket_name, file_name)
+
+    buffer = io.BytesIO()
+    object.download_fileobj(buffer)
+    inventory = pd.read_parquet(buffer)
+
+    samples_df = generate_seedfile(inventory, samples_list, source_bucket=seq_bucket)
+    samples_df.to_csv(output_file, index=False)
